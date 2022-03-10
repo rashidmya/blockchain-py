40582a92c3cd15e31cf36366d7e6d70433a8c6c0
buga-sys <itsfreak69@gmail.com>
2022-03-10 13:58:08 +0400

    proof to nonce

--- blockchain.py
+++ blockchain.py
@@ -9,13 +9,13 @@ from flask import Flask, jsonify
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.create_block(proof = 1, previous_hash = '0')
+        self.create_block(nonce = 1, previous_hash = '0')
 
-    def create_block(self, proof, previous_hash):
+    def create_block(self, nonce, previous_hash):
         block = {
             'index': len(self.chain) + 1,
             'timestamp': str(datetime.datetime.now()),
-            'proof': proof,
+            'nonce': nonce,
             'previous_hash': previous_hash
                  }
         self.chain.append(block)
@@ -24,16 +24,16 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, previous_proof):
-        new_proof = 1
-        check_proof = False
-        while check_proof is False:
-            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+    def nonce_of_work(self, previous_nonce):
+        new_nonce = 1
+        check_nonce = False
+        while check_nonce is False:
+            hash_operation = hashlib.sha256(str(new_nonce**2 - previous_nonce**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
-                check_proof = True
+                check_nonce = True
             else:
-                new_proof += 1
-        return new_proof
+                new_nonce += 1
+        return new_nonce
 
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
@@ -46,9 +46,9 @@ class Blockchain:
             previous_block = chain[index - 1]
             if block['previous_hash'] != self.hash(previous_block):
                 return False
-            previous_proof = previous_block['proof']
-            proof = block['proof']
-            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            previous_nonce = previous_block['nonce']
+            nonce = block['nonce']
+            hash_operation = hashlib.sha256(str(nonce**2 - previous_nonce**2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
             index += 1
@@ -68,11 +68,11 @@ blockchain = Blockchain()
 def mine_block():
     previous_block = blockchain.get_previous_block()
 
-    previous_proof = previous_block['proof']
+    previous_nonce = previous_block['nonce']
     previous_hash = blockchain.hash(previous_block)
 
-    proof = blockchain.proof_of_work(previous_proof)
-    block = blockchain.create_block(proof, previous_hash)
+    nonce = blockchain.nonce_of_work(previous_nonce)
+    block = blockchain.create_block(nonce, previous_hash)
 
     response = {'message':'Congratulations, you just mined a block!', **block}
     return jsonify(response), 200
